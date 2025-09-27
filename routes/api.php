e03fb4350dd875cce779747f1598670c255bb1bc
StarisbornT <crywallett556@gmail.com>
2025-09-27 16:34:06 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('login', [App\Http\Controllers\Auth\AuthController::class, 'login']);
     Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
